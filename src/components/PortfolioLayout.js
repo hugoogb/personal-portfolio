cc924d66f1ffac88bf74c03cc32e2b2761f1676d
Hugo GB <hugogaben8.02@gmail.com>
2023-04-07 17:28:14 +0200

    add footer

--- src/components/PortfolioLayout.js
+++ src/components/PortfolioLayout.js
@@ -4,17 +4,18 @@ import { HomeSection } from "./HomeSection.js";
 import { AboutSection } from "./AboutSection.js";
 import { ContactSection } from "./ContactSection.js";
 import { Header } from "./Header.js";
+import { Footer } from "./Footer.js";
 
 export function PortfolioLayout() {
 	return (
-		<main>
+		<>
 			<Navbar></Navbar>
 			<Header></Header>
 			<HomeSection></HomeSection>
 			<AboutSection></AboutSection>
 			<ProjectsSection></ProjectsSection>
 			<ContactSection></ContactSection>
-			{/* FOOTER ??? */}
-		</main>
+			<Footer></Footer>
+		</>
 	);
 }
