e9859f29ff341086ca8166817f129b8f42640cf7
Hugo GB <hugogaben8.02@gmail.com>
2023-04-07 18:49:48 +0200

    added font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import styles from "../styles/Header.module.css";
+import localFont from "next/font/local";
+
+const cyGroteskGrandBold = localFont({
+	src: [
+		{
+			path: "../pages/fonts/Cy.Grotesk/Cy Grotesk Grand Bold.ttf",
+			weight: "700",
+			style: "normal",
+		},
+	],
+});
 
 export function Header() {
-	const name = "Hugo García Benjumea";
+	const name = "Hugo Garcia Benjumea";
 
 	return (
-		<header>
+		<header className={cyGroteskGrandBold.className}>
 			<h1 className={styles.nameHeader}>{name}</h1>
 		</header>
 	);
