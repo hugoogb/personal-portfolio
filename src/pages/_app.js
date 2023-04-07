b56f7b72c17b8d73113f124c48742349520c1e17
Hugo GB <hugogaben8.02@gmail.com>
2023-04-07 18:48:13 +0200

    added fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<main className={raleway.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
