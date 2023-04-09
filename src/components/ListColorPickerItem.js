b545d0e836be64d7b6e2f16e1cb6fb85a09b1bd6
Hugo GB <hugogaben8.02@gmail.com>
2023-04-09 18:16:09 +0200

    add buttons and delete default button style

--- src/components/ListColorPickerItem.js
+++ src/components/ListColorPickerItem.js
@@ -3,12 +3,12 @@ import styles from "../styles/ColorPicker.module.css";
 export function ListColorPickerItem({ color }) {
 	return (
 		<li>
-			<a className={styles.listColorPickerItemLink}>
+			<button className={styles.listColorPickerItemLink}>
 				<div
 					className={styles.listColorPickerItem}
 					style={{ backgroundColor: color }}
 				></div>
-			</a>
+			</button>
 		</li>
 	);
 }
