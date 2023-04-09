70b01d948859bb82583901732bfc04fe02dcaa57
Hugo GB <hugogaben8.02@gmail.com>
2023-04-09 18:06:32 +0200

    updated styles in colorPicker

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -3,7 +3,15 @@ import styles from "../styles/ColorPicker.module.css";
 import { ListColorPickerItem } from "./ListColorPickerItem";
 
 export function ColorPicker() {
-	const colors = ["Red", "Blue", "Green", "Yellow", "Orange", "Purple"];
+	const colors = [
+		"#ff0000",
+		"#0000ff",
+		"#008000",
+		"#ffff00",
+		"#ffa500",
+		"#800080",
+		"#ffc0cb",
+	];
 	const colorsMapped = colors.map((color) => {
 		return <ListColorPickerItem key={color} color={color} />;
 	});
--- src/components/ListColorPickerItem.js
+++ src/components/ListColorPickerItem.js
@@ -2,8 +2,13 @@ import styles from "../styles/ColorPicker.module.css";
 
 export function ListColorPickerItem({ color }) {
 	return (
-		<li className={styles.listColorPickerItem}>
-			<a>{color}</a>
+		<li>
+			<a className={styles.listColorPickerItemLink}>
+				<div
+					className={styles.listColorPickerItem}
+					style={{ backgroundColor: color }}
+				></div>
+			</a>
 		</li>
 	);
 }
