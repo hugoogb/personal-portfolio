a410073a976ceb481d221718c6b7d21af741789e
Hugo GB <hugogaben8.02@gmail.com>
2023-04-14 22:27:17 +0200

    added styles to navbar

---
+++ src/components/NavItem.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import styles from "../styles/modules/Navbar.module.css";
+import { ColorContext } from "./PortfolioLayout";
+
+export const NavItem = ({ id, activeId, setActiveId, children }) => {
+	const color = useContext(ColorContext);
+
+	const spanStyles = Object.assign(
+		{},
+		{ backgroundColor: color },
+		{ width: activeId === id ? "35px" : "0" }
+	);
+
+	return (
+		<li className={styles.navItem}>
+			<a
+				href={"#" + children.toLowerCase()}
+				onClick={() => setActiveId(id)}
+			>
+				{children}
+				<span style={spanStyles} className={styles.navItemBar}></span>
+			</a>
+		</li>
+	);
+};
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import styles from "../styles/modules/Navbar.module.css";
 import { ColorPicker } from "./ColorPicker";
 import { Logo } from "./Logo";
+import { NavItem } from "./NavItem";
 
 export function Navbar({ setColor }) {
+	const [activeId, setActiveId] = useState(0);
+
+	const navItems = [
+		{ id: 0, name: "Home" },
+		{ id: 1, name: "About" },
+		{ id: 2, name: "Projects" },
+		{ id: 3, name: "Contact" },
+	];
+
+	const navItemsMapped = navItems.map((item) => {
+		return (
+			<NavItem
+				key={item.id}
+				id={item.id}
+				activeId={activeId}
+				setActiveId={setActiveId}
+			>
+				{item.name}
+			</NavItem>
+		);
+	});
+
 	return (
 		<nav className={styles.navbar}>
 			<div className={styles.navbarContainer}>
 				<Logo end={false}></Logo>
-				<ul className={styles.ulNavbar}>
-					<li>Home</li>
-					<li>About</li>
-					<li>Projects</li>
-					<li>Contact</li>
-				</ul>
+				<ul className={styles.ulNavbar}>{navItemsMapped}</ul>
 				<Logo end={true}></Logo>
 			</div>
 			<ColorPicker setColor={setColor}></ColorPicker>
