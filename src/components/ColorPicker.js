66fd5639a48c567e90744e933895b22394ec6f33
Hugo GB <hugogaben8.02@gmail.com>
2023-04-14 17:18:59 +0200

    using react-colorful as ColorPicker

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,51 +1,28 @@
 import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 import styles from "../styles/ColorPicker.module.css";
-import { ListColorPickerItem } from "./ListColorPickerItem";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ColorContext } from "./PortfolioLayout";
+import { HexColorPicker } from "react-colorful";
 
 export function ColorPicker({ setColor }) {
-	const colors = [
-		"#ff0000",
-		"#0000ff",
-		"#008000",
-		"#ffff00",
-		"#ffa500",
-		"#800080",
-		"#ffc0cb",
-	];
-
-	const handleClickChangeTheme = (color) => {
-		setColor(color);
-		handleSettingsClick();
-	};
-
-	const colorsMapped = colors.map((color) => {
-		return (
-			<ListColorPickerItem
-				key={color}
-				color={color}
-				onClick={handleClickChangeTheme}
-			/>
-		);
-	});
-
-	const [display, setDisplay] = useState("none");
-
-	const handleSettingsClick = () => {
-		display === "none" ? setDisplay("flex") : setDisplay("none");
-	};
+	const color = useContext(ColorContext);
+	const [display, setDisplay] = useState(false);
 
 	return (
-		<div className={styles.colorPickerContainer}>
-			<ul
-				className={styles.listColorPicker}
-				style={{
-					display: display,
-				}}
+		<div className={styles.pickerContainer}>
+			<div
+				style={{ display: display ? "inline-block" : "none" }}
+				className={styles.colorPickerContainer}
 			>
-				{colorsMapped}
-			</ul>
-			<Cog6ToothIcon onClick={setColor} className={styles.settingsIcon} />
+				<HexColorPicker
+					color={color}
+					onChange={setColor}
+				></HexColorPicker>
+			</div>
+			<Cog6ToothIcon
+				onClick={() => setDisplay(display ? false : true)}
+				className={styles.settingsIcon}
+			/>
 		</div>
 	);
 }
