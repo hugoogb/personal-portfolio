d779c62aa7e579ca1498871e8581c13f47d6a623
Hugo GB <hugogaben8.02@gmail.com>
2023-04-14 20:44:34 +0200

    add vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Raleway } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }) {
 	return (
 		<main className={raleway.className}>
 			<Component {...pageProps} />
+			<Analytics />
 		</main>
 	);
 }
