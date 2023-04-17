a6dde43248a1b17a97930df3054be991ac1bfa1a
Hugo GB <hugogaben8.02@gmail.com>
2023-04-17 02:42:36 +0200

    added meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,12 @@ export default function Index() {
 					name='viewport'
 					content='width=device-width, initial-scale=1'
 				/>
+				<meta
+					name='description'
+					content="Hugo's personal portfolio webpage showing his projects
+					 and a description on the work he does and how he does it, 
+					 the website also has a contact form and ways to downoald Hugo's resume."
+				></meta>
 				<link rel='icon' href='' />
 			</Head>
 			<PortfolioLayout></PortfolioLayout>
