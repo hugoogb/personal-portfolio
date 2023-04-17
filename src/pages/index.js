1595b73de36877c6a108baac903c241de7019560
Hugo GB <hugogaben8.02@gmail.com>
2023-04-17 02:04:54 +0200

    error fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
-import { Html } from "next/document.js";
 import Head from "next/head";
 import { PortfolioLayout } from "../components/PortfolioLayout.js";
 
 export default function Home() {
 	return (
 		<>
-			<Html lang='en'>
-				<Head>
-					<title>hugoogb</title>
-					<meta
-						name='viewport'
-						content='width=device-width, initial-scale=1'
-					/>
-					<link rel='icon' href='' />
-				</Head>
-			</Html>
+			<Head>
+				<title>hugoogb</title>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+				<link rel='icon' href='' />
+			</Head>
 			<PortfolioLayout></PortfolioLayout>
 		</>
 	);
