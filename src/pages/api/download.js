a6fc9d2e3ff372d72a9d96b81880c1d6deb323b0
Hugo GB <hugogaben8.02@gmail.com>
2023-04-19 00:08:13 +0200

    fix download api route

--- src/pages/api/download.js
+++ src/pages/api/download.js
@@ -1,10 +1,11 @@
 import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
 	const { fileName } = req.query;
-	const filePath = `/uploads/${fileName}`;
+	const filePath = path.join("public", "uploads", fileName);
 	const fileContents = fs.readFileSync(filePath);
 	res.setHeader("Content-disposition", `attachment; filename=${fileName}`);
-	res.setHeader("Content-Type", "application/octet-stream");
+	res.setHeader("Content-Type", "application/pdf");
 	res.send(fileContents);
 }
