a29c3902705343e380084deef8e6a4de80fcdac5
Hugo GB <hugogaben8.02@gmail.com>
2023-04-22 00:19:27 +0200

    add i18n packages and config

---
+++ i18n.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslation from "./locales/en.json";
+import esTranslation from "./locales/es.json";
+import caTranslation from "./locales/ca.json";
+
+i18n.use(initReactI18next).init({
+	lng: "en",
+	fallbackLng: "en",
+	// debug: true,
+	resources: {
+		en: {
+			translation: enTranslation,
+		},
+		es: {
+			translation: esTranslation,
+		},
+		ca: {
+			translation: caTranslation,
+		},
+	},
+});
+
+export default i18n;
