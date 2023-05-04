3acebf24681a8bb7ade38c0b9e5381adfa4811db
Hugo GB <hugogaben8.02@gmail.com>
2023-05-04 18:36:25 +0200

    fix favicon sources

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,19 +19,19 @@ export default function Index() {
 				<link
 					rel='apple-touch-icon'
 					sizes='180x180'
-					href='/favicon/apple-touch-icon.png'
+					href='/apple-touch-icon.png'
 				></link>
 				<link
 					rel='icon'
 					type='image/png'
 					sizes='32x32'
-					href='/favicon/favicon-32x32.png'
+					href='/favicon-32x32.png'
 				></link>
 				<link
 					rel='icon'
 					type='image/png'
 					sizes='16x16'
-					href='/favicon/favicon-16x16.png'
+					href='/favicon-16x16.png'
 				></link>
 				<link rel='manifest' href='/site.webmanifest'></link>
 				<link rel='canonical' href='https://hugoogb.dev'></link>
