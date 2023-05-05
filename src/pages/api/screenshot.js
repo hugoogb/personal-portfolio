766ae11b645db3035388e3a843207982df0ae448
Hugo GB <hugogaben8.02@gmail.com>
2023-05-05 23:19:44 +0200

    use browserless to fix endpoint serverless

--- src/pages/api/screenshot.js
+++ src/pages/api/screenshot.js
@@ -1,65 +1,35 @@
-import puppeteer from "puppeteer-core";
-import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer";
 
-/** The code below determines the executable location for Chrome to
- * start up and take the screenshot when running a local development environment.
- *
- * If the code is running on Windows, find chrome.exe in the default location.
- * If the code is running on Linux, find the Chrome installation in the default location.
- * If the code is running on MacOS, find the Chrome installation in the default location.
- * You may need to update this code when running it locally depending on the location of
- * your Chrome installation on your operating system.
- */
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-const exePath =
-	process.platform === "win32"
-		? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-		: process.platform === "linux"
-		? "/usr/bin/google-chrome"
-		: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+const getBrowser = () =>
+	IS_PRODUCTION
+		? // Connect to browserless so we don't run Chrome on the same hardware in production
+		  puppeteer.connect({
+				browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_API_TOKEN}`,
+		  })
+		: // Run the browser locally while in development
+		  puppeteer.launch({ headless: "new" });
 
-async function getOptions(isDev) {
-	let options;
-	if (isDev) {
-		options = {
-			args: [],
-			executablePath: exePath,
-			headless: true,
-		};
-	} else {
-		options = {
-			args: chrome.args,
-			executablePath: await chrome.executablePath,
-			headless: chrome.headless,
-		};
-	}
-	return options;
-}
+async function takeScreenshot(url) {
+	let browser = null;
 
-async function takeScreenshot(url, isDev) {
-	// get options for browser
-	const options = await getOptions(isDev);
+	browser = await getBrowser();
 
-	// launch a new headless browser with dev / prod options
-	const browser = await puppeteer.launch(options);
 	const page = await browser.newPage();
 
-	// set the viewport size
 	await page.setViewport({
 		width: 1280,
 		height: 720,
 	});
 
-	// tell the page to visit the url
-	await page.goto(url);
+	await page.goto(url, { waitUntil: "networkidle0" });
 
-	// take a screenshot
 	const screenshot = await page.screenshot({
 		type: "webp",
 		clip: { x: 0, y: 0, width: 1280, height: 720 },
 	});
 
-	// close the browser
 	await browser.close();
 
 	return screenshot;
@@ -68,12 +38,7 @@ async function takeScreenshot(url, isDev) {
 export default async function handler(req, res) {
 	const { url } = req.query;
 
-	// pass in the isDev=true parameter if you are developing locally
-	// to ensure puppeteer picks up your machine installation of
-	// Chrome via the configurable options
-	const isDev = req.query.isDev === "true";
-
-	const screenshot = await takeScreenshot(url, isDev);
+	const screenshot = await takeScreenshot(url);
 
 	res.setHeader("Content-Type", "image/png");
 	res.send(screenshot);
