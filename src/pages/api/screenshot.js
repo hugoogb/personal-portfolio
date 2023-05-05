763060b4da6978e1fcf8c665cbf54ac5b47449b5
Hugo GB <hugogaben8.02@gmail.com>
2023-05-05 17:53:47 +0200

    created endpoint to screenshot the urlPreview of a project

---
+++ src/pages/api/screenshot.js
@@ -0,0 +1,18 @@
+import puppeteer from "puppeteer";
+
+async function takeScreenshot(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setViewport({ width: 1280, height: 720 });
+	await page.goto(url);
+	const screenshot = await page.screenshot();
+	await browser.close();
+	return screenshot;
+}
+
+export default async function handler(req, res) {
+	const { url } = req.query;
+	const screenshot = await takeScreenshot(url);
+	res.setHeader("Content-Type", "image/png");
+	res.send(screenshot);
+}
