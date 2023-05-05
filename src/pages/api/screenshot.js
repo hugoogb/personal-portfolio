77ad1d6f3d18490d1a21dac3c7c39824d9a4678b
Hugo GB <hugogaben8.02@gmail.com>
2023-05-05 21:05:29 +0200

    use capture-website to fix serverless function

--- src/pages/api/screenshot.js
+++ src/pages/api/screenshot.js
@@ -1,18 +1,19 @@
-import puppeteer from "puppeteer";
+import captureWebsite from "capture-website";
 
 async function takeScreenshot(url) {
-	const browser = await puppeteer.launch({ headless: "new" });
-	const page = await browser.newPage();
-	await page.setViewport({ width: 1280, height: 720 });
-	await page.goto(url);
-	const screenshot = await page.screenshot();
-	await browser.close();
+	const screenshot = await captureWebsite.buffer(url, {
+		width: 1280,
+		height: 720,
+		type: "png",
+	});
+
 	return screenshot;
 }
 
 export default async function handler(req, res) {
 	const { url } = req.query;
 	const screenshot = await takeScreenshot(url);
+
 	res.setHeader("Content-Type", "image/png");
 	res.send(screenshot);
 }
