f963637fcbdd7f6628fe4c554a45f48efb8e79c6
Hugo GB <hugogaben8.02@gmail.com>
2024-10-08 00:11:00 +0200

    fix download CV

--- src/pages/api/download.js
+++ src/pages/api/download.js
@@ -2,24 +2,24 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-	let { fileName, language } = req.query;
-	fileName = path.basename(fileName); // sanitize fileName
-	const filePath = path.join(process.cwd(), "public", "uploads", fileName);
+  let { fileName, language } = req.query;
+  fileName = path.basename(fileName); // sanitize fileName
+  const filePath = path.join(process.cwd(), "public", "uploads", fileName);
 
-	try {
-		if (fs.existsSync(filePath)) {
-			const fileContents = fs.readFileSync(filePath);
-			res.setHeader(
-				"Content-disposition",
-				`attachment; filename=CV-Hugo_García_Benjumea-lang_${language}.pdf`
-			);
-			res.setHeader("Content-Type", "application/pdf");
-			res.send(fileContents);
-		} else {
-			res.status(404).send("File not found");
-		}
-	} catch (error) {
-		console.error(error);
-		res.status(500).send("Internal server error");
-	}
+  try {
+    if (fs.existsSync(filePath)) {
+      const fileContents = fs.readFileSync(filePath);
+      res.setHeader(
+        "Content-disposition",
+        `attachment; filename=CV-Hugo_Garcia_Benjumea-lang_${language}.pdf`
+      );
+      res.setHeader("Content-Type", "application/pdf");
+      res.send(fileContents);
+    } else {
+      res.status(404).send("File not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
 }
